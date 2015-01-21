439d4b9e6134d23c857a77b0d87865de486da5b8
Zachary Friss <friss@frissdesigns.com>
2015-01-21 00:34:32 -0500

    Basic dropwizard implementation

---
+++ src/main/java/com/friss/example/service/config/ExampleServiceConfiguration.java
@@ -0,0 +1,19 @@
+package com.friss.example.service.config;
+
+import com.yammer.dropwizard.config.Configuration;
+
+import javax.validation.Valid;
+
+public class ExampleServiceConfiguration extends Configuration {
+
+    @Valid
+    private MessagesConfiguration messages;
+
+    public MessagesConfiguration getMessages() {
+        return messages;
+    }
+
+    public void setMessages(MessagesConfiguration messages) {
+        this.messages = messages;
+    }
+}
---
+++ src/main/java/com/friss/example/service/hello/HelloResource.java
@@ -0,0 +1,22 @@
+package com.friss.example.service.hello;
+
+import com.friss.example.service.config.MessagesConfiguration;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path(value = "/hello")
+public class HelloResource {
+
+    private final MessagesConfiguration conf;
+
+    public HelloResource(MessagesConfiguration conf) {
+        this.conf = conf;
+    }
+
+    @GET
+    public String sayHello() {
+        return conf.getHello();
+    }
+
+}
